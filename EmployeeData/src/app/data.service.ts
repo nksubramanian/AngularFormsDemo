51939f90263f50c0eb9e92afcd01cb38c22b2392
subramanian <subramanian.enkay@gmail.com>
2024-07-14 16:44:15 +0530

    added service

--- EmployeeData/src/app/data.service.ts
+++ EmployeeData/src/app/data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { DataItem } from './data.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,36 @@ export class DataService {
 
   public name:string = "subramanian";
   constructor() { }
+
+  private data: DataItem[] = [];
+  private currentId = 0;
+
+  create(value: string): DataItem {
+    const newItem: DataItem = { id: this.currentId++, value };
+    this.data.push(newItem);
+    return newItem;
+  }
+
+  read(): DataItem[] {
+    return this.data;
+  }
+
+  update(id: number, newValue: string): DataItem | undefined {
+    const item = this.data.find(d => d.id === id);
+    if (item) {
+      item.value = newValue;
+    }
+    return item;
+  }
+
+  delete(id: number): boolean {
+    const index = this.data.findIndex(d => d.id === id);
+    if (index !== -1) {
+      this.data.splice(index, 1);
+      return true;
+    }
+    return false;
+  }
+
+
 }
